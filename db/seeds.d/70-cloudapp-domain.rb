310b506ddba7ff09b321491d601789fff4bc77cc
Eric D Helms <eric.d.helms@gmail.com>
2017-03-23 09:59:21 -0400

    Fixes #18627 - Look up unscoped domains

--- db/seeds.d/70-cloudapp-domain.rb
+++ db/seeds.d/70-cloudapp-domain.rb
@@ -1,4 +1,4 @@
-domain = Domain.where(:name => 'cloudapp.net').first_or_create
+domain = Domain.unscoped.where(:name => 'cloudapp.net').first_or_create
 if domain.nil? || domain.errors.any?
   raise "Unable to create domain: #{format_errors domain}"
 end
